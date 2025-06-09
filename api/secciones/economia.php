8935e1efff0f363d357dfe7ca4273a08f224ed56
Luiggi2000 <luiggiybanez@gmail.com>
2025-06-09 12:14:27 -0500

    Update economia.php

--- api/secciones/economia.php
+++ api/secciones/economia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Precio del cobre alcanza máximos de dos años impulsado por la demanda China",
+"autor" => "Bloomberg",
+"resumen" => "El metal rojo experimentó un notable incremento en su valor en los mercados internacionales, beneficiado por una mayor actividad industrial en China y expectativas de recortes de tasas en EE.UU.",
 ];
 ?>

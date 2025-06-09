a1d693a6740f712a796b7267e811c7068230afbb
Luiggi2000 <luiggiybanez@gmail.com>
2025-06-09 12:14:04 -0500

    Update nacional.php

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Gobierno peruano anuncia medidas para impulsar la reactivación económica",
+"autor" => "Agencia Andina",
+"resumen" => "El Ministerio de Economía y Finanzas presentó un paquete de medidas que incluyen incentivos fiscales y simplificación de trámites para dinamizar la inversión y el empleo en el país.",
 ];
 ?>

9e839743ee604f1803f9afa7c811a60f838fb336
Luiggi2000 <luiggiybanez@gmail.com>
2025-06-09 12:13:08 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "India concluye las elecciones generales más grandes del mundo con victoria de Modi",
+"autor" => "Reuters",
+"resumen" => "El primer ministro Narendra Modi se encamina a un tercer mandato, aunque con una mayoría más ajustada de lo esperado, tras las elecciones generales en India que duraron más de un mes y medio.",
 ];
 ?>
